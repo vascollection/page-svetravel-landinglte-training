66c1b101b6633122ef4260babdda47ced1972093
vascollection <michaelinaaay@gmail.com>
2023-08-18 16:35:45 +0800

    hello

---
+++ src/components/LocationCard.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let title
+    export let content
+    export let link
+    export let featuredImg
+    export let alt
+
+</script>
+
+<div class="card rounded-lg overflow-hidden relative">
+    <img class="absolute top-0" src={featuredImg} {alt}>
+    <div class="relative z-10">
+        <p>{title}</p>
+        <p>{content}</p>
+        <p>{link}</p>
+    </div>
+
+
+ 
+</div>
+
+<style>
+    .card{
+        width: 280px;
+        height: 452px;
+        background-color: white;
+    }
+</style>
